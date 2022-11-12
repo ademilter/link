00b2b8326d283e0df6cb922500843c25bc58040a
Adem ilter <ademilter@gmail.com>
2022-11-12 23:16:29 +0300

    create

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 
 import AnalyticsWrapper from "./analytics";
 import { Inter } from "@next/font/google";
+import { getCurrentUser } from "@/lib/session";
+import UserInfo from "@/components/user-info";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -10,11 +12,17 @@ const inter = Inter({
   subsets: ["latin-ext"],
 });
 
-export default function Layout({ children }) {
+export default async function Layout({ children }) {
+  const user = await getCurrentUser();
+
   return (
     <html lang="tr" className={`"scroll-smooth" ${inter.variable}`}>
       <body className="antialiased bg-white text-zinc-600 dark:bg-zinc-900 dark:text-zinc-400">
-        <div className="p-10">{children}</div>
+        <header className="p-6 border-b border-solid border-gray-100">
+          <UserInfo user={user} />
+        </header>
+
+        <div className="p-6">{children}</div>
 
         <AnalyticsWrapper />
       </body>
